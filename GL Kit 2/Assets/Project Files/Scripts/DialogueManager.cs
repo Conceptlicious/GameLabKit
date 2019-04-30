8e599685c0da397f377c4575a4066287f6881f8d
mongar23 <bevertje01@icloud.com>
2019-04-30 10:13:39 +0200

    Prototype room 4
    +Conveyerbelt movement
    +Dialogue
    +Quiz

---
+++ GL Kit 2/Assets/Project Files/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class DialogueManager : MonoBehaviour
+{
+	#region Varibles
+	public Text dialogueText;
+
+	[SerializeField]QuestionManager questionManager;
+
+	FileStream fileStream;
+	StreamReader reader;
+	string currentDialogue;
+	int currentDialogueIndex = -1;
+	#endregion
+
+	private void Awake()
+	{
+		Read();
+	}
+
+	public void Read()
+	{		
+		currentDialogueIndex++;
+
+		if (currentDialogueIndex <= 13 && !questionManager.needsAwnser)
+		{
+			fileStream = File.OpenRead(Path.Combine(LoadingPaths.PATH_DIALOGUE, LoadingPaths.FILE_NAME_DIALOGUE) + currentDialogueIndex + LoadingPaths.FILE_TYPE);
+			reader = new StreamReader(fileStream);
+
+			using (fileStream) using (reader)
+			{
+				currentDialogue = reader.ReadToEnd();
+				Write(currentDialogue);
+			}
+		}
+		else
+		{
+			Debug.LogWarning("Dialogue can not be loaded.");
+		}
+	}
+
+	private void Write(string displayedDialogue)
+	{
+		if (displayedDialogue.StartsWith("Q:"))
+		{
+			dialogueText.color = Color.cyan;
+			questionManager.needsAwnser = true;
+		}
+		else
+		{
+			dialogueText.color = Color.white;
+		}
+
+		dialogueText.text = displayedDialogue;
+	}
+}
