b10482ef05c505f4b28bb840c537316c53490f22
Noah <noahpbrinkman@gmail.com>
2019-05-13 15:27:57 +0200

    Auto stash before merge of "Development" and "origin/Development"

--- GL Kit 2/Assets/5.Scripts/Mini-Games/Room 3/GridMapStruct.cs
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/Room 3/GridMapStruct.cs
@@ -3,14 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Room3
+[Serializable]
+public struct GridMapStruct
 {
-    [Serializable]
-    public class GridMapStruct
-    {
-        public Texture2D GridMap;
-        public Color32[] TubeColours;
-        public Color32 AccessibleColour;
-        public Color32 InAccessibleColour;
-    }
+	#region Tooltip Map
+	[Tooltip
+	("The grid itself, make sure you that the start points and endpoints are the same colour with different alphas " +
+	"and that the colours match the colours in the list below")]
+	#endregion
+	public Texture2D Map;
+	#region Tooltip Colours
+	[Tooltip("The colours of your tubes make sure that these are the same as the colours in your gridmap")]
+	#endregion
+	public Tile.Group[] Groups;
+	public Color32 Inaccessible;
+	public Color32 Accessible;
+	public Color32 BridgeColor;
+	#region Tooltip Alpha Cut of
+	[Tooltip("Maximum Alpha necessary before it becomes an end point"), Range(0, 255)]
+	#endregion
+	public byte AlphaCutoff;
+
+	public Tile.Group GetGroupFromColor(Color32 color)
+	{
+		foreach(Tile.Group colorGroup in Groups)
+		{
+			if(!colorGroup.GroupColor.CompareIgnoreAlpha(color))
+			{
+				continue;
+			}
+
+			return colorGroup;
+		}
+
+		return Tile.Group.Ungroupped;
+	}
+
+	public Tile.Type GetTypeFromColor(Color32 color)
+	{
+		if (Accessible.Compare(color))
+		{
+			return Tile.Type.Connection;
+		}
+		if (Inaccessible.Compare(color))
+		{
+			return Tile.Type.Obstacle;
+		}
+		foreach(Tile.Group colorGroup in Groups)
+		{
+			if (colorGroup.GroupColor.CompareIgnoreAlpha(color))
+			{
+				if(color.a < AlphaCutoff)
+				{
+					return Tile.Type.EndPoint;
+				}else
+				{
+					return Tile.Type.StartPoint;
+				}
+			}
+		}
+		return Tile.Type.Obstacle;
+	}
 }
+
+
