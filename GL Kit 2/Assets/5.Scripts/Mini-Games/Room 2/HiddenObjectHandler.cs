24fbaec1b12872a8f34b7429d30a1e587b7b53cf
Mathias Bevers <bevertje01@icloud.com>
2019-05-27 11:04:54 +0200

    Auto stash before merge of "Development" and "origin/Development"

---
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/Room 2/HiddenObjectHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using GameLab;
+
+public class HiddenObjectHandler : BetterMonoBehaviour
+{
+	private const string objectAlreadyFoundMessage = "You have already found this object";
+	private const string wonMiniGameMessage = "You won the mini-game!";
+	private const int amountOfHiddenObjects = 8;
+	private static List<GameObject> foundObjects = new List<GameObject>();
+
+	public void ObjectFound(GameObject foundObject)
+	{
+		HiddenObject currentHiddenObject = foundObject.GetComponent<HiddenObject>();
+
+		if (foundObjects.Count == amountOfHiddenObjects)
+		{
+			WonMiniGame();
+		}
+		else if (!foundObjects.Contains(foundObject))
+		{
+			foundObject.SetActive(true);
+			foundObjects.Add(foundObject);
+
+			Debug.Log(foundObjects.Count);
+
+			TextUpdater.Instance.CallUpdateTextCoroutine(foundObject.name,
+				currentHiddenObject.Description);
+		}
+		else
+		{
+			TextUpdater.Instance.CallUpdateTextCoroutine(objectAlreadyFoundMessage, string.Empty);
+		}
+	}
+
+	private void WonMiniGame()
+	{
+		TextUpdater.Instance.CallUpdateTextCoroutine(wonMiniGameMessage, string.Empty);
+
+		NextRoomEvent nextRoomEvent = new NextRoomEvent();
+		EventManager.Instance.RaiseEvent(nextRoomEvent);
+	}
+}
---
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/Room 2/TextUpdater.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using GameLab;
+
+public class TextUpdater : Singleton<TextUpdater>
+{
+	private Text foundObjectNameText = null;
+	private Text foundObjectDisciptionText = null;
+	[SerializeField] private const int timeObjectInformationShown = 10;
+
+	private void Start()
+	{
+		SetValues();
+	}
+
+	public void CallUpdateTextCoroutine(string foundObjectName, string foundObjectDisciption)
+	{
+		StartCoroutine(UpdateText(foundObjectName, foundObjectDisciption));
+	}
+
+	private IEnumerator UpdateText(string foundObjectName, string foundObjectDisciption)
+	{
+		foundObjectNameText.text = foundObjectName;
+		foundObjectDisciptionText.text = foundObjectDisciption;
+
+		yield return new WaitForSeconds(timeObjectInformationShown);
+
+		foundObjectNameText.text = string.Empty;
+		foundObjectDisciptionText.text = string.Empty;
+	}
+
+	private void SetValues()
+	{
+		foundObjectNameText = transform.Find("ObjectNameText").GetComponent<Text>();
+		foundObjectDisciptionText = transform.Find("ObjectDescriptionText").GetComponent<Text>();
+
+		foundObjectNameText.text = string.Empty;
+		foundObjectDisciptionText.text = string.Empty;
+	}
+}
