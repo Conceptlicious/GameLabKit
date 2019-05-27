3d413c89861517230c007d5fcafbeea1ef56a2eb
Josh van Asten <joshmathew.va@gmail.com>
2019-05-27 14:13:27 +0200

    Backgrounds and Popups
    + Added Room 3's background
    + Fixed some functionality in creating popups

--- GL Kit 2/Assets/5.Scripts/UIPopUpManager.cs
+++ GL Kit 2/Assets/5.Scripts/UIPopUpManager.cs
@@ -7,6 +7,7 @@ using GameLab;
 public class UIPopUpManager : MonoBehaviour
 {
     [SerializeField] private DialogueObject dialogueObject;
+    [SerializeField] private Canvas parentCanvas;
 
     public void Start()
     {
@@ -28,7 +29,14 @@ public class UIPopUpManager : MonoBehaviour
         //layout visible
         string path = Settings.PATH_PREFABS + Settings.OBJ_NAME_UI_POPUP;
         Debug.Log(path);
-        Canvas UICanvas = Resources.Load<Canvas>(path);
+        Canvas UICanvas = GameObject.Instantiate(Resources.Load<Canvas>(path));
+        //GameObject.Instantiate(UICanvas);
+        if (UICanvas == null)
+        {
+            Debug.Log("UICanvas is null");
+        }
+        UICanvas.name = "hello";
+        
         UIPopUp popup = UICanvas.GetComponent<UIPopUp>();
         Debug.Assert(popup != null, Settings.ERR_ASSERT_UI_POPUP_MISSING_COMPONENT);
         UIPopUp.PopupFields data = popup.PopupFieldsData;
@@ -36,9 +44,9 @@ public class UIPopUpManager : MonoBehaviour
         data.title.text = dialogueObject.GetTextAndIterate();
         data.body.text = dialogueObject.GetTextAndIterate();
 
-        data.inputField.enabled = dialogueObject.GetTextAndIterate().IsNullOrEmpty() ? true : false;
+        data.inputField.enabled = !dialogueObject.GetTextAndIterate().IsNullOrEmpty();
          
-        string[] buttonNames = dialogueObject.GetTextAndIterate().Split(' ');
+       /* string[] buttonNames = dialogueObject.GetTextAndIterate().Split(' ');
         for (int i = 0; i < buttonNames.Length; i++)
         {
             GameObject newButton = GameObject.Instantiate(data.button.gameObject, UICanvas.transform);
@@ -46,11 +54,11 @@ public class UIPopUpManager : MonoBehaviour
             newPosition.x += (newButton.GetComponent<RectTransform>().rect.width + data.buttonPadding * i);
             newButton.transform.position = newPosition;
             newButton.GetComponentInChildren<Text>().text = buttonNames[i];
-        }
+        }*/
     }
 
     public void OnEventMarkerCall()
     {    //Don't uncomment this until a bug is fixed
-        //CreatePopUp();
+        CreatePopUp();
     }
 }
