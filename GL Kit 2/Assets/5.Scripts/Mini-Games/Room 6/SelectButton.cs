df7602231e8b620d802fe3a557e6d28167af0c2f
Mathias Bevers <bevertje01@icloud.com>
2019-09-04 16:53:22 +0200

    White room trophys fully dragable and snapable

---
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/Room 6/SelectButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using GameLab;
+
+public class SelectButton : BetterMonoBehaviour
+{
+	private Image buttonImage;
+	private Button selectButton;
+
+	private void Start()
+	{
+		selectButton = GetComponent<Button>();
+		buttonImage = GetComponent<Image>();
+
+		selectButton.onClick.AddListener(() => SelectSprite(buttonImage.sprite));
+	}
+
+	private void SelectSprite(Sprite selectedSprite)
+	{
+		SendSpriteRoom6.Instance.TubeSeleced(selectedSprite);
+	}
+}
---
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/WhiteRoom/TrophyGridManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameLab;
+
+[DisallowMultipleComponent]
+public class TrophyGridManager : Manager<TrophyGridManager>
+{
+	private List<TrophySocket> trophySockets = new List<TrophySocket>();
+
+	private void Start()
+	{
+		foreach(Transform child in transform)
+		{
+			TrophySocket trophySocketToAdd = child.GetComponent<TrophySocket>();
+
+			if(trophySocketToAdd != null)
+			{
+				trophySockets.Add(trophySocketToAdd);
+			}
+		}
+	}
+
+	public TrophySocket GetThrophySocketUnder(RectTransform rectTransform)
+	{
+		foreach(TrophySocket trophySocket in trophySockets)
+		{
+			if (RectTransformUtility.RectangleContainsScreenPoint(trophySocket.transform as RectTransform, rectTransform.position))
+			{
+				return trophySocket;
+			}
+		}
+
+		return null;
+	}
+}
