db2dc9bff2bc00627981841150b6aff78af41ba6
Mathias Bevers <bevertje01@icloud.com>
2019-09-05 14:18:25 +0200

    Recovery Room 1

---
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/Room 6/Room6TrophySelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GameLab;
+
+public class Room6TrophySelect : BetterMonoBehaviour
+{
+	[SerializeField] private Image selectedStyle;
+	[SerializeField] private Image trophy;
+
+
+	public void ConfirmSelection()
+	{
+		Debug.Log("Confirmed Succesfully");
+		trophy.overrideSprite = selectedStyle.sprite;
+		//trophy.color = new Color32(255, 0, 0, 255);
+	}
+
+	public void NewSelection(Image selecedImage)
+	{
+		selectedStyle = selecedImage;
+	}
+
+}
