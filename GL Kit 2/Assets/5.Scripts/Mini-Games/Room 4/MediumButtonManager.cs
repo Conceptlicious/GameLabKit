45402c0ef2b4898747c206108a8b6b35eb565cd6
Mathias Bevers <bevertje01@icloud.com>
2019-09-18 17:00:27 +0200

    Levels done
    TODO
    - Win condition
    - Display pros and cons

---
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/Room 4/MediumButtonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using GameLab;
+
+[DisallowMultipleComponent]
+public class MediumButtonManager : Manager<MediumButtonManager>
+{
+    [HideInInspector] public int activeButtons = 0;
+    private List<Button> buttons = new List<Button>();
+
+    private void Start()
+    {
+        foreach(Button button in GetComponentsInChildren<Button>())
+        {
+            buttons.Add(button);
+            button.interactable = false;
+        }
+
+        EnableNextButton();
+    }
+
+    public void EnableNextButton()
+    {
+        buttons[activeButtons].interactable = true;
+        ++activeButtons;
+    }
+}
--- GL Kit 2/Assets/5.Scripts/Mini-Games/Room 4/PuzzleManager.cs
+++ GL Kit 2/Assets/5.Scripts/Mini-Games/Room 4/PuzzleManager.cs
@@ -7,49 +7,64 @@ using GameLab;
 [DisallowMultipleComponent]
 public class PuzzleManager : Manager<PuzzleManager>
 {
-	public List<GameObject> Puzzles { get; private set; } = new List<GameObject>();
-	public List<PuzzlePieceDrag> PuzzlePieces { get; private set; } = new List<PuzzlePieceDrag>();
-	public List<PuzzlePieceSocket> PuzzlePieceSockets { get; private set; } = new List<PuzzlePieceSocket>();
-	public int NumberOfPuzzlePiecesNeeded { get; private set; } = 0;
-	private GameObject activePuzzle = null;
+    [SerializeField] private Text pieceText;
+    [HideInInspector] public int piecesInSocket = 0;
+    public List<GameObject> Puzzles { get; private set; } = new List<GameObject>();
+    public List<PuzzlePieceDrag> PuzzlePieces { get; private set; } = new List<PuzzlePieceDrag>();
+    public List<PuzzlePieceSocket> PuzzlePieceSockets { get; private set; } = new List<PuzzlePieceSocket>();
+    public int PiecesInSocketNeeded { get; private set; } = 0;
+    private GameObject activePuzzle = null;
 
-	private void Start()
-	{
-		foreach (Transform child in transform)
-		{
-			Puzzles.Add(child.gameObject);
-		}
-	}
+    private void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            Puzzles.Add(child.gameObject);
+        }
+    }
 
-	public void SetupNewPuzzle(GameObject puzzleToSetup)
-	{
-		PuzzlePieces.Clear();
-		PuzzlePieceSockets.Clear();
+    public void SetupNewPuzzle(GameObject puzzleToSetup)
+    {
+        PuzzlePieces.Clear();
+        PuzzlePieceSockets.Clear();
 
-		foreach(PuzzlePieceDrag puzzlePieceDrag in puzzleToSetup.GetComponentsInChildren<PuzzlePieceDrag>())
-		{
-			PuzzlePieces.Add(puzzlePieceDrag);
-		}
+        foreach (PuzzlePieceDrag puzzlePieceDrag in puzzleToSetup.GetComponentsInChildren<PuzzlePieceDrag>())
+        {
+            PuzzlePieces.Add(puzzlePieceDrag);
+        }
 
-		foreach(PuzzlePieceSocket puzzlePieceSocket in puzzleToSetup.GetComponentsInChildren<PuzzlePieceSocket>())
-		{
-			PuzzlePieceSockets.Add(puzzlePieceSocket);
-			++NumberOfPuzzlePiecesNeeded;
-		}
-	}
+        foreach (PuzzlePieceSocket puzzlePieceSocket in puzzleToSetup.GetComponentsInChildren<PuzzlePieceSocket>())
+        {
+            PuzzlePieceSockets.Add(puzzlePieceSocket);            
+        }
 
-	public List<PuzzlePieceSocket> GetPuzzlePieceSocketsUnder(RectTransform rectTransform)
-	{
-		List<PuzzlePieceSocket> puzzlePieceSocketsUnder = new List<PuzzlePieceSocket>();
+        PiecesInSocketNeeded = PuzzlePieceSockets.Count;
+        piecesInSocket = 0;
+    }
 
-		foreach(PuzzlePieceSocket puzzlePieceSocket in PuzzlePieceSockets)
-		{
-			if(RectTransformUtility.RectangleContainsScreenPoint(puzzlePieceSocket.transform as RectTransform, rectTransform.position))
-			{
-				puzzlePieceSocketsUnder.Add(puzzlePieceSocket);
-			}
-		}
+    public List<PuzzlePieceSocket> GetPuzzlePieceSocketsUnder(RectTransform rectTransform)
+    {
+        List<PuzzlePieceSocket> puzzlePieceSocketsUnder = new List<PuzzlePieceSocket>();
 
-		return puzzlePieceSocketsUnder;
-	}
+        foreach (PuzzlePieceSocket puzzlePieceSocket in PuzzlePieceSockets)
+        {
+            if (RectTransformUtility.RectangleContainsScreenPoint(puzzlePieceSocket.transform as RectTransform, rectTransform.position))
+            {
+                puzzlePieceSocketsUnder.Add(puzzlePieceSocket);
+            }
+        }
+
+        return puzzlePieceSocketsUnder;
+    }
+
+    public void DisplayPieceText(string pieceString, bool textNeedsToBeEmpty = false)
+    {
+        if(textNeedsToBeEmpty)
+        {
+            pieceText.text = string.Empty;
+            return;
+        }
+
+        pieceText.text = pieceString;
+    }
 }
