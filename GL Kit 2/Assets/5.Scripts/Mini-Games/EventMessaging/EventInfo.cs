e13b590364aa3b0e2e823fdf0ab74de1cccc8a9a
Mobjunk <34821598+Mobjunk@users.noreply.github.com>
2020-12-16 12:32:51 +0100

    Clean branch

--- GL Kit 2/Assets/5.Scripts/Mini-Games/EventMessaging/EventInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using CustomEventCallbacks;
-using UnityEngine;
-
-public abstract class EventInfo
-{
-    public string eventDescription = EventSystem.DESC_EVENT_GENERIC;
-}
--- GL Kit 2/Assets/Resources/Text/Dialogue/Dutch/InkDialogue/Frontdoor.ink
+++
@@ -1,4 +0,0 @@
-===Intro===
--"Hallo! Mijn naam is Dr. Gamelab."
--"Ik begrijp dat u interesse heeft in serious gaming. Nou, dan bent u aan het juiste adres!"
-->END
